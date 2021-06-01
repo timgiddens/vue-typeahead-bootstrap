16cbd2bfa25fab4639a357601d54432798ed029c
Tim Giddens <tim.giddens@clevyr.com>
2021-06-01 16:02:03 -0500

    temporarily remove auto select on enter

--- src/components/VueTypeaheadBootstrapList.vue
+++ src/components/VueTypeaheadBootstrapList.vue
@@ -287,9 +287,6 @@ export default {
       evt.preventDefault()
     },
     hitActiveListItem() {
-      if (this.activeListItem < 0) {
-        this.selectNextListItem();
-      }
       if (this.activeListItem >= 0) {
         this.$emit('hit', this.matchedItems[this.activeListItem])
       }
